84b6c186c57b2805cf8a3683ce291a2bc1adb5ff
parichehrBoreshnavard <cherboren@gmail.com>
2023-01-02 12:48:58 -0500

    add allfiles

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using ArtGalleryApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ArtGalleryApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Events()
+        {
+            return View();
+        }
+        public IActionResult Artists()
+        {
+            return View();
+        }
+        public IActionResult ArtistsAccount()
+        {
+            return View();
+        }
+        public IActionResult ArtistPage()
+        {
+            return View();
+        }
+        public IActionResult ArtworkPage()
+        {
+            return View();
+        }
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
